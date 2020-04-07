f0854d9bfbc740e11e7747fae95a12973065832a
Walter Lai <walterlaicollege@gmail.com>
2020-04-07 17:45:36 -0400

    alerts user when there is < 1 subject/ group/ study

--- app.r
+++ app.r
@@ -4,10 +4,26 @@ library(ggplot2)
 library(lme4)
 # testing version control should nt appear in master
 #replace with link to github csv
-dataset=read.csv("C:/Users/Walt/Desktop/datascience/quantmed/clinicalTrialSim/experimentSimulator/studies2.csv")
+#dataset=read.csv("C:/Users/Walt/Desktop/datascience/quantmed/clinicalTrialSim/experimentSimulator/studies2.csv")
+#
+dataset=read.csv("https://raw.githubusercontent.com/wlai0611/experimentSimulator/tabs/studies2.csv")
 ui=fluidPage(
    tabsetPanel
-      (
+      ( tabPanel("Introduction",
+                 verticalLayout(
+                   helpText("The objective of this app is to help a researcher determine
+                            the inclusion criteria for an experiment given a hypothesized effect size.
+                            
+                            In the first tab, \"Data\" , the user selects the inclusion criteria.
+                            In the second tab, \"Graph\", the user observes the predicted result of the similated
+                            experiment. 
+                            
+                            The \"simulated experiment\" is a mixed linear model fitted on
+                            the filtered data.
+                            ")
+                                )
+        
+                ),
         tabPanel("Data",
           sidebarLayout(
               sidebarPanel(
@@ -18,9 +34,11 @@ ui=fluidPage(
                           ),
               mainPanel(
                 helpText("Select ranges for the inclusion criteria of the simulated study.
-                         This will filter the table of studies shown below.  Each row represents
-                         a study participant.  Click \"Simulate\" to apply the filter to the table.
-                         Click on the \"Graph\" tab to see the expected experiment outcome. "),
+                         This will filter the table of research subjects shown below.
+                         Click \"Simulate\" to apply the filter to the table.
+                         Click on the \"Graph\" tab to see the expected experiment outcome.
+                         The \"study\" column represents which study a research subject belongs to.
+                         The \"score\" column represents the dependent variable."),
                 tableOutput("subData")
                       )
           )),
@@ -34,8 +52,9 @@ ui=fluidPage(
                      
                      
                      
-                     
-                     plotOutput("modelResult")
+                     helpText("The formula: score ~ group + (1|study) "),
+                     plotOutput("modelResult"),
+                     tableOutput("subjectCheck")
                    )
                  )
         )
@@ -46,8 +65,16 @@ server=function(input,output){
   output$data=renderTable({dataset})
   isolate({data=reactiveValues(df=dataset)
   subDataset=reactiveValues(df=dataset)
+  subjectCheck=reactiveValues(tbl= table(subDataset[['df']][,"study"],subDataset[['df']][,"group"]) )
+  
   })
-
+  output$subjectCheck=renderTable({
+    #tbl=subjectCheck[['tbl']]
+   # length(tbl[tbl>0])
+   # subjectCheck[['tbl']][subjectCheck[['tbl']]>0]<length(unique(subDataset[['df']][,"study"]))*length(unique((subDataset[['df']][,"group"])))
+    subjectCheck[['tbl']]
+  })
+  
 
   observeEvent(input$simulate,
                isolate({
@@ -60,10 +87,15 @@ server=function(input,output){
                                                      data[['df']]["Age"]>=input$ageRange[1]& 
                                                      data[['df']]["Age"]<=input$ageRange[2]
                                                      ,]
-                  if(nrow(subDataset[['df']])==0)
+                  subjectCheck[['tbl']]= table(subDataset[['df']][,"study"],subDataset[['df']][,"group"]) 
+                  
+                  if(nrow(subDataset[['df']])==0 |
+  length(subjectCheck[['tbl']][subjectCheck[['tbl']]>0])<(length(unique(data[['df']][,"study"]))*length(unique((data[['df']][,"group"]))))
+                     
+                  )
                   {  showModal(modalDialog(
                       title = "Important message",
-                      "There are no studies that match this inclusion criteria/ study design!"
+                      "Please widen range. There are not enough research subjects that fit your inclusion criteria."
                   ))}else{
                     df=subDataset[['df']]
                     n = sum(df[,"group"]=="Control")-1
@@ -85,7 +117,7 @@ server=function(input,output){
                  {message("An error occurred:\n", e)
                    showModal(modalDialog(
                      title = "Important message",
-                     "There are no studies that match this inclusion criteria/ study design!"
+                     "Please widen range. There are no studies that match this inclusion criteria/ study design!"
                    ))}
                  )
               
@@ -97,7 +129,7 @@ server=function(input,output){
     plotIntercepts=function()
       {if(length(unique(subDataset[['df']][,'group']))<2){
         showModal(modalDialog("","Please widen range.  The current filtering criteria
-                              has less than two subjects. Not enough to simulate an experiment."))
+                              has Not enough research subjects to simulate an experiment."))
       }else if(length(unique(subDataset[['df']][,'study']))>1)
       {out=lmer(score~group+(1|study),data=subDataset[['df']])
       outCI=confint(out)
@@ -109,7 +141,7 @@ server=function(input,output){
       #X2.5..
       ggplot(outCI)+geom_errorbar(aes(x=group,ymin=X2.5..,ymax=X97.5..))+
         ylab("Outcome Measure")+labs(caption = "Error bars represent 95% confidence intervals
-                                     for Mean Score on Outcome Measure.  This was a mixed model
+                                     for the Population Mean Score of each group.  This was a mixed model
                                      with a random effect of study.  Because Subjects from similar studies may 
                                      have similar scores.")
         } else{
